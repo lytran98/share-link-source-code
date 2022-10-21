955a984accb0ea603bf0bef00f0df6f21546c732
lytran98 <96611139+lytran98@users.noreply.github.com>
2022-10-21 23:54:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-<h1>ly</h1>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title> Link Source Code Các Dự Án Website </title>
+	<link rel="stylesheet" type="text/css" href="css/ly.css">
+</head>
+<body>
+	<h1>Danh Sách Các Dự Án Được Chia Sẻ Dưới Đây</h1>
+<br>
+<h2>1. Dự án hệ thống quản lý và kiểm kê POS với mã vạch</h2>
+<a href="http://traffic1s.com/p7jJ" class="button button1">Tải Xuống</a>
+<a href="https://www.youtube.com/channel/UCI1Ez4VYaqEu2KB-JE376gw">Xem Hướng Dẫn Cài Đặt</a>
+<h2>2. Ứng dụng mạng xã hội giống Facebook</h2>
+<a href="http://traffic1s.com/Hd7Ey" class="button button1">Tải Xuống</a> 
+<a href="https://www.youtube.com/watch?v=synqTWSgOhg&lc=Ugxcy-o7iRybMdmgDRh4AaABAg">Xem Hướng Dẫn Cài Đặt</a>
+<h2>3. Ứng dụng đặt đồ ăn giống như Shopee Food</h2>
+<a href="http://traffic1s.com/htND" class="button button1">Tải Xuống</a>
+<a href="https://www.youtube.com/channel/UCI1Ez4VYaqEu2KB-JE376gw">Xem Hướng Dẫn Cài Đặt</a>
+<h2>4. Source code website bán hàng laptop, điện thoại, linh kiện</h2>
+<a href="http://traffic1s.com/t3TDTd8" class="button button1">Tải Xuống</a>
+<a href="https://www.youtube.com/channel/UCI1Ez4VYaqEu2KB-JE376gw">Xem Hướng Dẫn Cài Đặt</a>
+<h2>5. Source code website bán hàng giống Sendo</h2>
+<a href="http://traffic1s.com/F0IR136U" class="button button1">Tải Xuống</a>
+<a href="https://www.youtube.com/channel/UCI1Ez4VYaqEu2KB-JE376gw">Xem Hướng Dẫn Cài Đặt</a>
+
+</body>
+</html>
